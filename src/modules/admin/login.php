bc81008fb5b7191f699ee383c4906fd797b6c667
victorioso-daniel-2 <cscb.vpaudit.daniel@gmail.com>
2025-05-21 13:37:00 +0800

    arrange and rename the file for readability

--- src/modules/admin/login.php
+++ src/modules/admin/login.php
@@ -14,7 +14,7 @@ unset($_SESSION['error']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login - StackOverCash Bank</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./dashboard.css">
+    <link rel="stylesheet" href="./assets/css/login.css">
 </head>
 <body>
     <div class="container">
