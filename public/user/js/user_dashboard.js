82c570bd8166002857a810bddf5ecb520133b342
delumen-ivan <ivandelumen05@gmail.com>
2025-07-12 02:21:28 +0800

    Revert "fix the dashboard"
    This reverts commit 39690ab8902a8624472565489963ce0f23f3b5bd.

--- public/user/js/user_dashboard.js
+++ public/user/js/user_dashboard.js
@@ -360,10 +360,14 @@ function updateAccountDisplay() {
         if (account.account_number === selectedAccountNumber) {
             accountElement.classList.add('selected');
         }
-        accountElement.classList.add('pointer');
+        accountElement.style.cursor = 'pointer';
         accountElement.addEventListener('click', (e) => {
             // Prevent toggling mask when clicking the eye icon
             if (e.target.classList.contains('account-eye-icon') || e.target.closest('.account-eye-icon')) return;
+
+        accountElement.classList.add('pointer');
+        accountElement.addEventListener('click', () => {
+
             window.selectedAccountNumber = account.account_number;
             update_balance_display(account.balance);
             fetchRecentTransactions(account.account_number);
@@ -754,7 +758,7 @@ function displayFinancialTip() {
     
     tipContainer.innerHTML = `
         <div class="tip-header">
-            <i class="fas fa-lightbulb"></i>
+            <i class="fas ${tip.icon}"></i>
             <h3>Financial Tip</h3>
         </div>
         <div class="tip-content">
